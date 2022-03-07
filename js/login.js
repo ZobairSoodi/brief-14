f5858292475cd80185758a3a7c387d9f869e25d4
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-03-07 13:49:43 +0100

    ez

--- js/login.js
+++ js/login.js
@@ -5,21 +5,28 @@ var signup_form = document.getElementById("signup_form");
 
 login_btn.addEventListener("click", ()=>{
     signup_form.style.zIndex = -1;
+    login_form.style.opacity = 1;
+    signup_form.style.opacity = 0;
     login_btn.classList.remove("form_inactive_login");
     signup_btn.classList.add("form_inactive_signup");
+    login_btn.style.backgroundColor = "white";
+    if(login_btn.classList.contains("hover_button")){
+        login_btn.classList.remove("hover_button");
+    }
 })
-// login_btn.addEventListener("mouseover",()=>{
-//     if(login_btn.classList.contains("form_inactive_login")){
-//         login_btn.classList.add("hover_button");
-//     }
-// })
-// login_btn.addEventListener("mouseout",()=>{
-//     if(login_btn.classList.contains("form_inactive_login")){
-//         login_btn.classList.remove("hover_button");
-//     }
-// })
+signup_btn.addEventListener("click", ()=>{
+    signup_form.style.zIndex = 1;
+    signup_form.style.opacity = 1;
+    login_form.style.opacity = 0;
+    signup_btn.classList.remove("form_inactive_signup");
+    login_btn.classList.add("form_inactive_login");
+    signup_btn.style.backgroundColor = "white";
+    if(signup_btn.classList.contains("hover_button")){
+        signup_btn.classList.remove("hover_button");
+    }
+})
 function add_button_hover(btn1){
-    if(btn1.classList.contains("form_inactive_login")){
+    if(btn1.classList.contains("form_inactive_login")||btn1.classList.contains("form_inactive_signup")){
         btn1.classList.add("hover_button");
     }
 }
@@ -28,8 +35,3 @@ function remove_button_hover(btn1){
         btn1.classList.remove("hover_button");
     }
 }
-signup_btn.addEventListener("click", ()=>{
-    signup_form.style.zIndex = 1;
-    signup_btn.classList.remove("form_inactive_signup");
-    login_btn.classList.add("form_inactive_login");
-})
