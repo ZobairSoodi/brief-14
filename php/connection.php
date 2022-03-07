5778797a7dcebfb108604f79cd173fb09a11f798
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-03-07 23:52:33 +0100

    connection established hhhhhh.

---
+++ php/connection.php
@@ -0,0 +1,30 @@
+<?php
+    $conn = new mysqli("localhost", "root", "", "brief14");
+    if(isset($_POST["signup_submit"])){
+        $fname = $_POST["fname"];
+        $lname = $_POST["lname"];
+        $email = $_POST["email_signup"];
+        $pass = $_POST["pass_signup"];
+        $pass_confirm = $_POST["pass_confirm"];
+        $phone = $_POST["phone"];
+        $adress = $_POST["adress"];
+        
+        $sql = "INSERT INTO client (`prenom`, `nom`, `email`, `pass`, `telephone`, `adresse`) VALUES('$fname','$lname','$email','$pass','$phone','$adress')";
+        $conn->query($sql);
+    }
+    if(isset($_POST["login_submit"])){
+        $email = $_POST["email_login"];
+        $pass = $_POST["pass_login"];
+        $sql = "SELECT * FROM client WHERE email = '$email' AND pass = '$pass';";
+        $data = $conn->query($sql);
+        $res = $data->fetch_assoc();
+        if(!empty($res)){
+            foreach ($res as $key => $value) {
+                echo "$key = $value <br>";
+            }
+        }
+        else{
+            echo "User does not exist!";
+        }
+    }
+?>
