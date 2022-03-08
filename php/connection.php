2eb0a04c20b2751a0c96104118ca2ea4affd00b6
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-03-08 10:39:33 +0100

    dde

--- php/connection.php
+++ php/connection.php
@@ -8,9 +8,17 @@
         $pass_confirm = $_POST["pass_confirm"];
         $phone = $_POST["phone"];
         $adress = $_POST["adress"];
+        $sql = "SELECT * FROM client WHERE email = $email;";
+        $data = $conn->query($sql);
+        $res = $data->fetch_assoc();
+        if(empty($res)){
+            $sql = "INSERT INTO client (`prenom`, `nom`, `email`, `pass`, `telephone`, `adresse`) VALUES('$fname','$lname','$email','$pass','$phone','$adress')";
+            $conn->query($sql);
+        }
+        else{
+            
+        }
         
-        $sql = "INSERT INTO client (`prenom`, `nom`, `email`, `pass`, `telephone`, `adresse`) VALUES('$fname','$lname','$email','$pass','$phone','$adress')";
-        $conn->query($sql);
     }
     if(isset($_POST["login_submit"])){
         $email = $_POST["email_login"];
