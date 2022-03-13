6869a3ab14b484bfcaa152d23c05e78dee7609b3
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-03-13 06:40:29 +0100

    ds

--- order.php
+++ order.php
@@ -1,12 +1,12 @@
 <?php
     $quantity = $_POST["quantity"];
-    $idClient = $_GET["id"];
+    $id_product = $_GET["id"];
 
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "brief14";
-            
+    
     $conn = new mysqli($servername, $username, $password, $dbname);
     $sql_client = "SELECT * FROM client WHERE idClient = $idClient";
     $data = $conn->query($sql_client);
