be29a7a1635fb28225fdcec25650365109c0842f
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-03-13 08:34:47 +0100

    Update order.php

--- order.php
+++ order.php
@@ -1,20 +1,24 @@
 <?php
-    $quantity = $_POST["quantity"];
-    $id_product = $_GET["id"];
+    session_start();
+    foreach ($_SESSION as $key => $value) {
+        echo $value . "<br>";
+    }
+    // $quantity = $_POST["quantity"];
+    // $id_product = $_GET["id"];
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "brief14";
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $dbname = "brief14";
     
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    $sql_client = "SELECT * FROM client WHERE idClient = $idClient";
-    $data = $conn->query($sql_client);
-    $res = $data->fetch_assoc();
+    // $conn = new mysqli($servername, $username, $password, $dbname);
+    // $sql_client = "SELECT * FROM client WHERE idClient = $idClient";
+    // $data = $conn->query($sql_client);
+    // $res = $data->fetch_assoc();
 
-    $sql_insert_order = "INSERT INTO commande (`idClient`, `date`, `adresseLivraison`)
-            VALUES ($idClient, '".date("Y-m-d")."', '".$res["adresse"]."')
-    ";
-    echo $sql_insert_order;
-    // $conn->query($sql_insert_order);
+    // $sql_insert_order = "INSERT INTO commande (`idClient`, `date`, `adresseLivraison`)
+    //         VALUES ($idClient, '".date("Y-m-d")."', '".$res["adresse"]."')
+    // ";
+    // echo $sql_insert_order;
+    // // $conn->query($sql_insert_order);
 ?>
