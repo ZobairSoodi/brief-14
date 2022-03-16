df2537719b6c87fc53e51e985ee0d78378be2e51
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-03-16 13:11:09 +0100

    Update add_to_cart.php

--- php/add_to_cart.php
+++ php/add_to_cart.php
@@ -4,11 +4,107 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-        echo $_GET["id"];
-    ?>
+    
+
+    <section class="intro">
+        
+        <div class="gradient-custom-1 h-100">
+            <div class="firstdiv">
+                <p>Free shipping on any $35 purchase</p>
+            </div>
+
+            <nav style="position:sticky !important;">
+              <div>
+                  <div>
+                      LOGO
+                  </div>
+                  <div>
+                      <ul>
+                          <li>Home</li>
+                          <li>Products</li>
+                      </ul>
+                  </div>
+              
+              </div>
+          </nav>
+          
+            <h1 style="text-align: center; margin-top: 70px; color: #522D6D;">MY CART <i class="fa-solid fa-cart-shopping-fast"></i></h1>
+
+          <div class="mask d-flex align-items-center h-100">
+              
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-12">
+                  <div class="table-responsive bg-white">
+                    <table class="table mb-0">
+                      <thead>
+                        <tr>
+                          <th scope="col">Photo</th>
+                          <th scope="col">Price</th>
+                          <th scope="col">Units</th>
+                          <th scope="col">Sub total</th>
+                          <th scope="col">delivery</th>
+                          <th scope="col">Total</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td scope="row"><img src="prod.jpg" alt=""></td>
+                          <td>1200</td>
+                          <td>    <div class="control-part">
+                            <button class="num-control"><img src="minus.png"></button>
+                            <input type="text" id="unics-num">
+                            <button class="num-control"><img src="plus.png"></button>
+                        </div>
+                      </td>
+                          <td>61</td>
+                          <td>Edinburgh</td>
+                          <td>$320,800</td>
+                        </tr>
+
+
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer>
+        <div id="container">
+            <div id="part1">
+                <div id="companyinfo"> <a id="sitelink" href="#"  style="color: black;">Contact Us</a>
+                    <p id="title" style="color: black;">Cool and Perfect Snippet code</p>
+                    <p id="detail" style="color: black;">We create awesome code snippets that will help you in creating your own beautiful site. </p>
+                </div>
+                <div id="explore">
+                </div>
+                <div id="visit">
+    
+    
+                </div>
+                <div id="legal">
+                  
+                </div>
+                <div id="subscribe">
+                    <p id="txt4" style="color: black;">Subscribe to US</p>
+                    <form> <div id="Message"> <input id="email" type="email" placeholder="Email"> <br> <textarea rows="4" cols="21" name="comment"> </textarea> </div> <div id="information"> <input id="email" type="email" placeholder="Email"> <input id="email" type="email" placeholder="Email">  <input id="Submit" type="submit" value="Submit" ></div> </form> <a class="waves-effect waves-light btn"></a>
+                </div>
+            </div>
+            <div id="part2">
+                <p id="txt6"  style="color: black; text-align: center;" ><i class="material-icons tiny"> copyright</i>copyright 2019 BBbootstrap - All right reserved</p>
+            </div>
+        </div>
+    </footer>
+
 </body>
 </html>
