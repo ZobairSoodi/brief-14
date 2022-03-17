6aba95c95c7e46a5c0e73b167bd603e028c2bd7b
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-03-17 15:33:02 +0100

    Update remove_from_cart.php

--- php/remove_from_cart.php
+++ php/remove_from_cart.php
@@ -1,9 +1,26 @@
 <?php
     session_start();
-    for($i=0;$i<count($_SESSION["cart"]);$i++){
-        if($_SESSION["cart"][$i]->id_product==$_GET["id"]){
-            unset($_SESSION["cart"][$i]);
-        }
+    foreach ($_SESSION["cart"] as $key => $value) {
+        echo $key."<br>";
     }
-    header("location: add_to_cart.php");
+    // $_SESSION["cart"][3]->id_product;
+    // for($i=0;$i<count($_SESSION["cart"]);$i++){
+    //     if($_SESSION["cart"][$i]->id_product==$_GET["id"]){
+    //         unset($_SESSION["cart"][$i]);
+    //     }
+    // }
+    // header("location: add_to_cart.php");
+    // $list = [
+    //     "item1",
+    //     "item2",
+    //     "item3"
+    // ];
+    // for($i=0;$i<count($list);$i++){
+    //         if($list[$i]=="item2"){
+    //             unset($list[$i]);
+    //         }
+    //     }
+    // foreach ($list as $value) {
+    //     echo $value."<br>";
+    // }
 ?>
